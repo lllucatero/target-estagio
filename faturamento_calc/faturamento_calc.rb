3056981dd09b6c899e2e6d2c7a270ac2210d98c7
Lucas Pontes <lcspontes@gmail.com>
2022-06-15 20:27:15 -0300

    média dos faturamentos e número de dias maiores que a média

--- faturamento_calc/faturamento_calc.rb
+++ faturamento_calc/faturamento_calc.rb
@@ -22,6 +22,14 @@ def maior_faturamento()
   @faturamentos.max.round(2)
 end
 
-        
+def media_faturamentos()
+  faturamentos_validos = [] 
+  @faturamentos.each {|value| faturamentos_validos.push(value) if value > 0 }
+  @media_mensal = (faturamentos_validos.inject(0) {|sum, value| sum += value} / faturamentos_validos.length).round(2)
+end
 
+def maior_que_media()
+  mais_que_media = 0
+  @faturamentos.each {|value| mais_que_media += 1 if value > @media_mensal}
+  mais_que_media
 end
