e069198730371b9bb7f13ec0550c83f6b386f224
Lucas Pontes <lcspontes@gmail.com>
2022-06-17 15:46:18 -0300

    isolate menu method and create fatura_calc file

---
+++ faturamento_calc/fatura_calc.rb
@@ -0,0 +1,28 @@
+require_relative 'methods_fatura_calc.rb'
+require_relative 'menu.rb'
+
+loop do 
+  faturamentos_totais()
+  menu
+
+  operation = gets.chomp.to_i
+  case operation
+  when 1
+    print '|  O menor faturamento no mês foi de R$', menor_faturamento()
+    puts ''
+    puts '|--------------------------------------------------------------|'
+  when 2
+    print '|  O maior faturamento no mês foi de R$', maior_faturamento()
+    puts ''
+    puts '|--------------------------------------------------------------|'
+  when 3
+    print '|  A média de faturamento mensal foi de R$', media_faturamentos()
+    puts ''
+    print '|  Houve ', maior_que_media(), ' dias em que o faturamento superou a média mensal.'
+    puts ''
+    puts '|--------------------------------------------------------------|'
+  end
+
+  use_again()
+
+end
--- faturamento_calc/menu.rb
+++ faturamento_calc/menu.rb
@@ -1,28 +1,23 @@
-require_relative 'faturamento_calc.rb'
-require 'json'
-
 def menu
   
-  puts "+------------------------------------------------------------+"
-  puts "|  FATURA CALC                                               |"
-  puts "|------------------------------------------------------------|"
-  puts "|  1. Menor faturamento do mês;                              |"                               
-  puts "|  2. Maior faturamento do mês;                              |"
-  puts "|  3. Média de faturamento e dias acima da média;            |"
-  puts "+------------------------------------------------------------+"
-  print "|  Selecione a opção desejada: "
-  @option = gets.chomp.to_i
-  puts "|------------------------------------------------------------|"
+  puts '+--------------------------------------------------------------+'
+  puts '|  FATURA CALC                                                 |'
+  puts '|--------------------------------------------------------------|'
+  puts '|  1. Menor faturamento do mês;                                |'                               
+  puts '|  2. Maior faturamento do mês;                                |'
+  puts '|  3. Média de faturamento e dias acima da média;              |'
+  puts '+--------------------------------------------------------------+'
+  print '| Selecione a opção desejada: '
 
 end
-faturamentos_totais()
-menu
 
-case @option
-when 1
-  menor_faturamento()
-when 2
-  maior_faturamento()
-when 3
-  maior_que_media()
+def use_again
+  puts  '|                                                              |'
+  puts  '|  Deseja continuar?                                           |'
+  puts  '|  Pressione S para Sim                                        |'
+  puts  '|  Pressione N para Não                                        |'
+  option = gets.chomp
+  if option.upcase == "N"
+      exit
+  end
 end
